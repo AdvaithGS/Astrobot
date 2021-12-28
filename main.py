cfb39d3fc47735813caa54a5111ab4900e55aa01
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-28 21:48:12 +0530

    function does not work

--- main.py
+++ main.py
@@ -26,16 +26,7 @@ api_key2 = os.environ['api_key2']
 api_key3 = os.environ['api_key3']
 secret = os.environ['api_key4']
 appid = '6aaa6bf5-ecd9-44c6-9a30-8252e2269103'
-def send_all(message,type):
-  for guild in db.keys():
-        try:
-          channel = client.get_channel(db[guild])
-          if type == 1:
-            await channel.send(embed = message)
-          elif type == 2:
-            await channel.send(message)
-        except:
-          pass
+
 def get_activity():
   choice = random.choice([0,2,3,4,6,7,8,10,11])
   lst = ['With the stars','','The Sounds Of The Universe','Cosmos','With a bunch of Neutron stars','','Your .iss requests','How The Universe  Works','Life of A Star', '', 'Richard Feynman talk about bongos','Milky Way and Andromeda collide']
@@ -43,7 +34,16 @@ def get_activity():
   activity = lst[choice]
   choice = choice%4
   return activity , choice
-
+'''def send_all(message,type):
+    for guild in db.keys():
+        try:
+          channel = client.get_channel(db[guild])
+          if type == 1:
+            await channel.send(embed = message)
+          elif type == 2:
+            await channel.send(message)
+        except:
+          pass'''
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -53,7 +53,11 @@ async def on_ready():
   await client.change_presence(status = discord.Status.idle, activity = discord.Activity(name = activity , type = choice))
   '''embed = discord.Embed(title = 'Happy James Webb Space Telescope Launch Day!',description ="The James Webb Space Telescope—the most powerful space telescope ever made. This mission successfully lifted off at 7:20 a.m. EST (12:20 UTC), Dec. 25, 2021, aboard an Ariane 5 rocket from Europe’s Spaceport in French Guiana.\nWith revolutionary technology, Webb will observe a part of space and time never seen before, providing a wealth of amazing views into an era when the very first stars and galaxies formed–over 13.5 billion years ago.\nIt can explore our own solar system's residents with exquisite new detail and study the atmospheres of distant worlds.\nFrom new forming stars to devouring black holes, Webb will reveal all this and more! It’s the world’s largest and most powerful space telescope ever built, surely marking the start of a new age in humanity's exploration of our cosmos.",color = discord.Color.orange())
   embed.set_image(url = 'https://cdn.arstechnica.net/wp-content/uploads/2016/03/614445main_013526_white2.jpg')
-  send_all(embed,1)'''
+  for guild in db.keys():
+        try:
+          await channel.send(embed = embed)
+        except:
+          pass'''
 
 @client.event
 async def on_guild_join(guild):
@@ -144,7 +148,11 @@ async def on_message(message):
         await ctx.send('Never mind.')
       else:
         await ctx.send('Yes my lord.')
-        send_all('.daily',2)
+        for guild in db.keys():
+          try:
+            await channel.send('.daily')
+          except:
+            pass
   #removes a given channel from the apod service.
   elif message.content.startswith('.remove'):
     try:
@@ -434,7 +442,11 @@ async def on_message(message):
   parameters = {'date':strftime('%Y-%m-%d')}
   if db['apod'] != strftime('%Y-%m-%d') and int(strftime('%H')) > 4 and (requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}',params=parameters).text)[8:11] != '404':  
       db['apod'] = strftime('%Y-%m-%d')
-      send_all('.daily',2)
+      for guild in db.keys():
+        try:
+          await channel.send('.daily')
+        except:
+          pass
   
   if int(strftime('%H')) >= db['hour'] + 6 or int(strftime('%H')) < db['hour']:
     db['hour'] = int(strftime('%H'))
