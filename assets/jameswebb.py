b4cb57661149acd47b95a5ee30e2e4625037732f
= <advaith.gs4@gmail.com>
2021-12-31 01:16:50 +0530

    update jameswebb.py

--- assets/jameswebb.py
+++ assets/jameswebb.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from json import loads
-from assets.image import get_image
+try:
+  from assets.image import get_image
+except:
+  from image import get_image
 from time import strftime,gmtime
 def get_james_webb():
     d = int(strftime('%d',gmtime()))
     s = strftime(f'%Y/%m/{d-7}-%H:48:00',gmtime()) 
-    req = requests.get('https://jwst.nasa.gov/content/webbLaunch/whereIsWebb.html?s=09').text
+    req = requests.get('https://jwst.nasa.gov/content/webbLaunch/whereIsWebb.html?units=metric').text
     soup = BeautifulSoup(req,'lxml')
     page = soup.find_all('script')[6].text
     a = page[page.find(s)-100 :page.find(s)+230].replace(s,s.replace(':','/'))
@@ -16,7 +19,8 @@ def get_james_webb():
     hoursPassed = round((float(found['elapsedDays'])%1)*24)
     elapsedtime = f'{daysPassed} days and {hoursPassed} hours'
     fromEarth = float(found['distanceTravelledKm']) + float(found['velocityKmSec'])*(int(strftime('%M'))-48)*60
+    velocity = str(found['velocityKmSec']) + ' km/s'
     tol2 = round(1446320 - fromEarth,3)
     completion = round(fromEarth/1446320*100,3)
-    return elapsedtime,fromEarth,tol2,completion,get_image()
+    return elapsedtime,fromEarth,tol2,completion,get_image(),velocity
     #distance between Earth and l2 -1446320     
