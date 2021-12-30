5129abc9f39feafe2d519c5d5e092e2199351a7c
= <advaith.gs4@gmail.com>
2021-12-31 01:09:04 +0530

    update image.py

--- assets/image.py
+++ assets/image.py
@@ -33,20 +33,131 @@ The Left/Port (+J2) Sunshield Boom Deployment
 
 Nominal Event Time: Launch + 6 days
 
-The Port +J2 Mid-boom deployment steps include the completion of the sunshield cover roll up, the deployments team then extends the +J2 mid-boom along with the +J2 side of the five membranes. This operation is a motor-driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/112.png']
+The Port +J2 Mid-boom deployment steps include the completion of the sunshield cover roll up, the deployments team then extends the +J2 mid-boom along with the +J2 side of the five membranes. This operation is a motor-driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/112.png'],
+'6.5':['''Sunshield STARBOARD Mid-Boom
+The Right/Starboard (-J2) Sunshield Boom Deployment
 
+Nominal Event Time: Launch + 6 days
+
+The Starboard (-J2) Mid-boom deployment steps include the completion of the sunshield cover roll up, the deployments team then extends the -J2 mid-boom along with the -J2 side of the five sunshield membranes. This operation is a motor driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/113.png' ],
+'7':['''Sunshield Layer Tensioning
+Nominal Event Time: Launch + 7 days
+
+This operation is a multi-step two-day activity which completes the final membrane releases, as well as tensioning the five layers of the sunshield.
+
+After initial sunshield deployments of the forward and aft UPS (which carry the fully folded sunshield), followed by the port and starboard mid-booms (which support and initially unfold all 5 layers of the sunshield together), each layer is successively tensioned.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/114.png'],
+'8':['''Sunshield Tensioning Complete
+The Sunshield is Fully Deployed!
+
+Nominal Event Time: Launch + 8 days
+
+Tensioning of layer 1 (the hottest layer, closest to the sun) through layer 5 is complete which puts the sunshield in its fully deployed state.
+
+Temperatures on the Sun/hot side of the sunshield will reach a maximum of approximately 383K or approximately 230 degrees F and on the cold mirror/instruments side of the sunshield, a minimum of approximately 36K or around -394 degrees F. Due to the engineering of the sunshield, this incredible transition takes place across a distance of approximately six feet.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/115.png'],
+'10':["""Secondary Mirror Deployment
+This step begins the Secondary Mirror deployment phase.
+
+Nominal Event Time: Launch + 10 days
+
+The Secondary Mirror Support Structure (SMSS) is moved into its operational position. Webb's secondary mirror is at the end of this structure. As it is deployed, its long booms will swing the secondary mirror out in front of the primary mirror. The secondary mirror plays an important role in reflecting the light from the primary mirror to where the instruments sit, behind the primary mirror.
+""",'https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/116.png'],
+'10.5':["""
+Secondary Mirror Deployment
+Complete - the Secondary Mirror is fully deployed and latched.
+
+Nominal Event Time: Launch + 10 days
+
+Webb's secondary mirror is at the end of the Secondary Mirror Support Structure (SMSS). As it is deployed, its long booms will swing the secondary mirror out in front of the primary mirror. The secondary mirror plays an important role in reflecting the light from the primary mirror to where the instruments sit, behind the primary mirror.""",'https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/117.png'],
+'11':['''Aft Deployed Instrument Radiator
+Aft Deployed Instrument Radiator (ADIR)
+
+Nominal Event Time: Launch + 11 days
+
+This activity releases the last of four launch locks that holds the Aft Deployed Instrument Radiator (ADIR) in its launch configuration. Releasing the last device allows springs to drive the ADIR into its final deployed position. The first three launch locks were released just after launch to prevent any unwanted strain in the system as the ADIR and telescope cools.
+''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/118.png'],
+'12':['''Port Primary Mirror Wing
+This step begins the Primary Mirror deployment phase.
+
+Nominal Event Time: Launch + 12 days
+
+The deployments team begins planning and operations for the deployment of the left/port (+V2) primary mirror wing. This operation deploys and latches the +V2 wing of the primary mirror. Each wing holds three of the 18 mirror segments. This is a motor-driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/119.png'],
+'12.5':['''Port Primary Mirror Wing
+Complete - the Port Primary Mirror Wing is deployed and latched.
+
+Nominal Event Time: Launch + 12 days
+
+The left/port (+V2) wing of the primary mirror is fully deployed and latched. Each wing holds three of the 18 mirror segments. This is a motor-driven deployment.
+''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/120.png'],
+'13.1':['''Starboard Primary Mirror Wing
+Begin deployment of the Starboard Primary Mirror Wing.
+
+Nominal Event Time: Launch + 13 days
+
+The deployments team begins planning and operations to deploy the right/starboard (-V2) wing of the primary mirror. Each wing holds 3 of the 18 mirror segments. This is a motor driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/121.png'],
+'13.2':['''Starboard Primary Mirror Wing
+Complete - Deployment of the Starboard Primary Mirror Wing.
+
+Nominal Event Time: Launch + 13 days
+
+The right/starboard (-V2) wing of the primary mirror is fully deployed and latched. Each wing holds 3 of the 18 mirror segments. This is a motor driven deployment.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/122.png'],
+'13.3':['''WEBB IS FULLY DEPLOYED!
+Nominal Event Time: Launch + 13 days
+
+It will continue to travel to the second Lagrange point (L2) for another two weeks, at which point it will enter a large orbit around the L2 point. The following five months will be used to cool the telescope to operating temperature, fine-tune the mirror alignment, and calibrate the instruments.
+''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/123.png'],
+'15':['''Individual Mirror Segment Movements
+Nominal Event Time: Launch + 15-24 days
+
+This operation is a multi-day multi-step activity to activate and move each of its 18 primary mirror segments (which are adjustable) out of their launch configuration.
+
+The primary mirror segments and secondary mirror are moved by six actuators that are attached to the back of each mirror piece. The primary mirror segments also have an additional actuator at its center that adjusts its curvature. The telescope's tertiary mirror remains stationary.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/124.png'],
+'29':['''L2 Insertion Burn
+Mid Course Correction Burn (MCC2) - Begins L2 Insertion
+
+Nominal Event Time: Launch + 29 days
+
+A series of activites to plan and execute MCC2 - the insertion burn for Webb's L2 orbit. MCC2 corrects any residual trajectory errors and adjusts the final L2 orbit.
+
+The James Webb Space Telescope is launched on a direct path to an orbit around the second SunEarth Lagrange Point (L2), but it needs to make its own mid-course thrust correction maneuvers to get there. This is by design, because if Webb gets too much thrust from the Ariane rocket, it can’t turn around to thrust back toward Earth because that would directly expose its telescope optics and structure to the Sun, overheating them and aborting the science mission before it can even begin. Therefore, Webb gets an intentional slight under-burn from the Ariane and uses its own small thrusters and on-board propellant to make up the difference.
+
+There are three mid-course correction (MCC) maneuvers: MCC-1a, MCC-1b, and MCC-2. This final burn, MCC-2, which inserts Webb into its L2 halo orbit.''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/126.png'],
+'29.5':['''WEBB IS ORBITING L2
+Orbit Insertion Complete.
+Webb is Orbiting L2 in a HALO Orbit.
+
+Nominal Event Time: Launch + 29.5 days
+
+Webb is now orbiting L2. Telescope deployment is complete. Ongoing cooldown and instrument turn-on continue. The telescope and scientific instruments started to cool rapidly in the shade of the sunshield once it was deployed, but it will take several weeks for them to cool all the way down and reach stable temperatures. This cooldown will be carefully controlled with strategically-placed electric heater strips. The remaining five months of commissioning will be all about aligning the optics and calibrating the scientific instruments.
+
+''','https://www.jwst.nasa.gov/content/webbLaunch/assets/images/deployment/1000pxWide/127.png']
 }
 from time import strftime,gmtime
+from datetime import datetime
 def get_image():
     global db
-    d = str(int(strftime('%d',gmtime())) - 25)
-    if d in (3,5):
+    d = (datetime(int(strftime('%Y')) , int(strftime('%m')),int(strftime('%d'))) - datetime(2021,12,25)).days
+    if d in (3,5,6,10,12,29):
         h = strftime('%H',gmtime())
         if int(h) > 12:
-            return db[d+'.5']
+            return db[str(d)+'.5']
         return db[h]
+    elif d == 9:
+      return db['8']
+    elif d == 14:
+      return db['13.3']
+    elif d == 13:
+      h = int(strftime('%H',gmtime()))
+      if h <= 8:
+        return db[str(d)+'.1']
+      elif h <= 16:
+        return db[str(d) + '.2']
+      return db[str(d) + '.3']
+    elif d in range(15,29):
+      return db['15']
+    elif d > 29:
+      return db['29']
     else:
-        return db[d]
+        return db[str(d)]
     
 
 
