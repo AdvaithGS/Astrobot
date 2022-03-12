5200b1891efaeb5d9d68ac3960bbfa2ca6b73f53
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-12 23:22:27 +0530

    update database.py

--- assets/database/database.py
+++ assets/database/database.py
@@ -5,10 +5,15 @@ from hashlib import sha1
 github = Github(environ['api_key6'])
 repository = github.get_user().get_repo('db')
 filename = 'db'
+
+def contents():
+  return repository.get_contents(filename)
+
 def update(db):
   content = str(db)
-  sha = repository.get_contents(filename).sha
+  sha = contents().sha
   f = repository.update_file(filename, f"update file on {strftime('%H:%M %d/%m/%Y')}", content,sha)
+  
 def retrieve():
-  db = eval(repository.get_contents(filename).decoded_content.decode())
+  db = eval(contents().decoded_content.decode())
   return db
