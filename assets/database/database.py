6a6b28856f382442c756b699b702529628a02e91
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-12 23:02:51 +0530

    bot seems to be down

--- assets/database/database.py
+++ assets/database/database.py
@@ -8,7 +8,7 @@ filename = 'db'
 def update(db):
   content = str(db)
   sha = repository.get_contents(filename).sha
-  f = repository.update_file(filename, f"update file on {strftime('%H:%M %d/%m/%Y')}", content,sha )
+  f = repository.update_file(filename, f"update file on {strftime('%H:%M %d/%m/%Y')}", content,sha)
 def retrieve():
   db = eval(repository.get_contents(filename).decoded_content.decode())
   return db
