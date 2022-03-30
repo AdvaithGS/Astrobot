7d9d6e69e15d83dcf05c07124597f4275b1bcbeb
Advaith GS <advaith.gs4@gmail.com>
2022-03-30 11:41:14 +0530

    reverting to older

--- bot/assets/database/database.py
+++ bot/assets/database/database.py
@@ -6,19 +6,19 @@ repository = github.get_user().get_repo('db')
 filename = 'db'
 
 def contents(filename = 'db'):
-  #return repository.get_contents(filename)
+  return repository.get_contents(filename)
   pass
 
 def update(db,filename = 'db',remarks = f"update file on {strftime('%H:%M %d/%m/%Y')}"):
-  #content = str(db)
-  #sha = contents(filename).sha
-  #f = repository.update_file(filename, remarks, content,sha)
+  content = str(db)
+  sha = contents(filename).sha
+  f = repository.update_file(filename, remarks, content,sha)
   pass
   
 def retrieve(filename = 'db'):
-  #try:
-  #  db = eval(contents(filename).decoded_content.decode())
-  #except:
-  #  db = contents(filename).decoded_content.decode()
-  #return db
+  try:
+    db = eval(contents(filename).decoded_content.decode())
+  except:
+    db = contents(filename).decoded_content.decode()
+  return db
   pass
