72e6482810a053844394dde7fc65cc8e3d59794f
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-04-01 13:21:55 +0530

    fixed further issues with commands

--- bot/assets/countries/country_code.py
+++ bot/assets/countries/country_code.py
@@ -1,12 +1,13 @@
+import os
 def find_country(code):
-    target = ';'+code
-    try:
-      with open ('assets/countries/countries.txt') as f:
-          for line in f.readlines():
-              if target in line:
-                  return line.split(';')[0].title()
-    except:
-      with open('./countries.txt') as f:
-          for line in f.readlines():
-            if target in line:
-              return line.split(';')[0].title()       
+  os.chdir(os.getcwd().replace('facts','countries'))
+  try:
+    with open('countries.txt') as f:
+      lst = f.readlines()
+  except:
+    with open('assets/countries/countries.txt') as f:
+      lst = f.readlines()
+  target = ';'+code
+  for line in lst:
+    if target in line:
+      return line.split(';')[0].title()
--- bot/assets/facts/facts.py
+++ bot/assets/facts/facts.py
@@ -1,10 +1,11 @@
 import random
+import os
 def random_fact():
+  os.chdir(os.getcwd().replace('countries','facts'))
   try:
-    with open ('assets/facts/facts.txt',encoding = 'utf-8') as f:
+    with open ('./facts.txt',encoding='utf-8') as f:
         line = random.choice(f.readlines()).split(';')
-        return line
   except:
-    with open ('./facts.txt',encoding = 'utf-8') as f:
+    with open ('facts.txt',encoding='utf-8') as f:
         line = random.choice(f.readlines()).split(';')
-        return line
+  return line
