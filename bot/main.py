e5dada31b752e6b0e68f61a89d516db7785ee422
Advaith GS <advaith.gs4@gmail.com>
2022-04-12 15:13:12 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -526,7 +526,7 @@ async def on_message(message):
         try:
           channel = client.get_channel(db[guild])
           await channel.send('.daily')
-          embed = discord.Embed(title = 'Notice - Please reinvite the bot',description = 'Effective 1st May, Astrobot will be moving to slash commands and mention commands - `/help` and `@AstroBot help`, this requires server owners to reinvite the bot - without the need to kick AstroBot- due to Discord\'s new policy changes.Link can be taken from the bot\'s profile. Please make note of this change.',colour = discord.Colour.orange())
+          embed = discord.Embed(title = 'Notice - Please reinvite the bot - Ignore if already done or invited after April 6',description = 'Effective 1st May, Astrobot will be moving to slash commands and mention commands - `/help` and `@AstroBot help`, this requires server owners to reinvite the bot - without the need to kick AstroBot- due to Discord\'s new policy changes.Link can be taken from the bot\'s profile. Please make note of this change.',colour = discord.Colour.orange())
           await channel.send(embed = embed)
 
         except:
@@ -564,7 +564,7 @@ async def on_message(message):
       else:
         update(dict(db))
       if random.randrange(1,11) == 4:
-        embed = discord.Embed(title = 'Notice - Please reinvite the bot',description = 'Effective 1st May, Astrobot will be moving to slash commands and mention commands - `/help` and `@AstroBot help`, this requires server owners to reinvite the bot - without the need to kick AstroBot- due to Discord\'s new policy changes.Link can be found in the bot\'s profile. Please make note of this change.',colour = discord.Colour.orange())
+        embed = discord.Embed(title = 'Notice - Please reinvite the bot - Ignore if already done or invited after April 6',description = 'Effective 1st May, Astrobot will be moving to slash commands and mention commands - `/help` and `@AstroBot help`, this requires server owners to reinvite the bot - without the need to kick AstroBot- due to Discord\'s new policy changes.Link can be found in the bot\'s profile. Please make note of this change.',colour = discord.Colour.orange())
         ctx.send(embed = embed)
   except:
     pass
