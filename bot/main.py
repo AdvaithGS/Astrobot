5fa7c49755d68bbbb824103dd1c41b7ea67f149b
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-05-03 16:46:57 +0530

    added condition to print

--- bot/main.py
+++ bot/main.py
@@ -105,7 +105,8 @@ async def check_apod():
           embed = disnake.Embed(title=title, url=url,   description=desc,color=disnake.Color.orange())
           await channel.send(content = name)
       except Exception as e:
-        print(guild,e,'from 1')
+        if guild.isdigit():
+          print(guild,e,'from 1')
     update(dict(db))
 
 
