3e927a75bd6d080ac3f211ecf65f4a1a0e9b7c88
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-05-13 11:41:05 +0530

    renamed async method

--- bot/assets/loops/stats.py
+++ bot/assets/loops/stats.py
@@ -3,7 +3,7 @@ from time import mktime
 from disnake.ext import tasks
 
 @tasks.loop(hours = 24)
-async def stats(client,stats,update):
+async def stats_loop(client,stats,update):
   guilds = client.guilds
   now = mktime(datetime.now().timetuple())
   stats['member_guilds'].append([now,len(guilds)])
@@ -14,4 +14,4 @@ async def stats(client,stats,update):
   update(stats,'stats')
 
 def call_stats(client,stats,update):
-  stats.start(client,stats,update)
+  stats_loop.start(client,stats,update)
