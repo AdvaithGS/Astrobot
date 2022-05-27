e3a6b767eae0623edd26b31d449ed7cf5105a597
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-05-27 16:34:45 +0530

    checking ctx

--- bot/main.py
+++ bot/main.py
@@ -229,7 +229,8 @@ async def help(
 async def channel(ctx):
   '''
   Register for the automatic APOD subscription 
-  '''  
+  '''
+  print(dir(ctx),help(ctx.id))
   if db[ctx.guild.id] == ctx.id:
     embed = disnake.Embed(title = 'This server already has an APOD subscription',description = 'This channel had previously already been registered for the Astronomy Picture of The Day service.', color=disnake.Color.orange())
   else:
