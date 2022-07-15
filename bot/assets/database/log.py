0cc8a465de564861053b4d5de05bebdc038fb13b
Advaith GS <advaith.gs4@gmail.com>
2022-07-15 14:40:24 +0530

    Update log.py

--- bot/assets/database/log.py
+++ bot/assets/database/log.py
@@ -3,12 +3,6 @@ from time import mktime
 async def log_command(command,db,update):
   try: 
     db[command] += 1
-    if db['resetlast'] - mktime(datetime.now().timetuple()) >= 2592000:
-      db['resetlast'] = mktime(datetime.now().timetuple())
-      for i in ['daily','help','channel','remove','info','iss','fact','weather','phase','sky','webb']:
-          db[i] = 0
-      update(dict(db),'db','Database reset')
-
     else:
       update(dict(db))
   except Exception as e:
