18a64c0b14f3ba0ecf0cceb5228a226c9cd11ef6
Advaith GS <advaith.gs4@gmail.com>
2022-07-15 15:00:22 +0530

    Update log.py

--- bot/assets/database/log.py
+++ bot/assets/database/log.py
@@ -3,7 +3,6 @@ from time import mktime
 async def log_command(command,db,update):
   try: 
     db[command] += 1
-    else:
-      update(dict(db))
+    update(dict(db))
   except Exception as e:
     print(e,command,'from log command')
