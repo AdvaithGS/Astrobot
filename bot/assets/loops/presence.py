d01eadf52e5c24b439d8639233d952093e67756c
= <=>
2022-09-09 23:27:45 +0530

    adding presence

--- bot/assets/loops/presence.py
+++ bot/assets/loops/presence.py
@@ -9,18 +9,17 @@ activities = iter(cycle([[0, 'With the stars'], [2, 'The Sounds Of The Universe'
 @tasks.loop(hours = 6)
 async def set_activity(client,db,caller,update):
   global activities
-  
+
   activity = next(activities)
   #0 - playing 1- playing and twitch  2 - Listening 3 - Watching 4 -  5- competing
-  choice = activity[0]
-  desc = activity[1]
+
   with open('log.txt','a') as f:
     time = strftime('%d/%m/%Y-%H:%M')
-    f.write(f'\n{time} {caller}: {choice}-{desc}')
+    f.write(f'\n{time} {caller}: {activity[0]}-{activity[1]}')
   with open('log.txt','r') as f:
     update(f.read(),'logs')
   db['hour'] = mktime(datetime.now().timetuple())
-  await client.change_presence(status = disnake.Status.idle,activity = disnake.Activity(name = 'slash commands! Type /help! Reinvite the bot if that doesnt work.',type = 2))
+  await client.change_presence(status = disnake.Status.idle,activity = disnake.Activity(name = activity[0],type = activity[1]))
 
 def call_set_activity(client,db,caller,update):
   set_activity.start(client,db,caller,update)
