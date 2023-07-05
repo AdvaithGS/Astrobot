373d40c239bb27e9093f66ca6609c99ae39cfeba
AdvaithGS <advaith.gs4@gmail.com>
2023-07-05 22:22:33 +0530

    created solving.py

---
+++ bot/assets/astrometry/solving.py
@@ -0,0 +1,18 @@
+from requests import post
+from json import dumps
+from os import environ
+
+astro_key = environ['astro_key']
+
+def get_sub_id(url):
+  x = post('http://nova.astrometry.net/api/login', data = {'request-json' : dumps({'apikey':astro_key})}).json()
+
+  req = post('http://nova.astrometry.net/api/url_upload',
+     data = {
+       'request-json' : 
+          dumps(
+            { "session": x['session'], "url": url , "scale_units": "degwidth", "scale_lower": 0.1, "scale_upper": 180.0,"downsample_factor" : 2}
+              )}).json()
+  return req['subid']
+
+
