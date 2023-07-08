be4b245c087818be2969a5c348a53cbc1da3d0d9
AdvaithGS <advaith.gs4@gmail.com>
2023-07-08 12:49:19 +0530

    reducing dependencies

--- bot/main.py
+++ bot/main.py
@@ -10,6 +10,7 @@ from assets.loops.top import update_guilds
 from assets.loops.stats import call_stats
 from assets.database.database import update,retrieve
 from assets.tools.apod import apod
+from keep_alive import keep_alive
 db = retrieve()
 with open('log.txt','w') as f:
   f.write(retrieve('logs'))
@@ -178,5 +179,5 @@ async def on_message(message):
     await check_job_status()
   
 
-
+keep_alive()
 client.run(environ['TOKEN'])
