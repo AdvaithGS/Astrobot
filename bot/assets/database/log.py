eee60e8167bac21cb9b028e68994ab21e4e785c6
AdvaithGS <advaith.gs4@gmail.com>
2023-07-14 20:01:58 +0530

    update log.py

--- bot/assets/database/log.py
+++ bot/assets/database/log.py
@@ -1,9 +1,7 @@
-import disnake
-async def log_command(command,db : dict,update,ctx : disnake.User):
-  if ctx.user.id == 756496844867108937:
+from assets.database.database import retrieve,update
+async def log_command(command,id : int ):
+  if id == 756496844867108937:
     return
-  try: 
-    db[command] = db.get(command,0) + 1
-    update(dict(db))
-  except Exception as e:
-    print(e,command,'from log command')
+  db = retrieve('db')
+  db[command] = db.get(command,0) + 1
+  update(db,'db')
