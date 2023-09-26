6278c307e78ad72ff2c33e60cf3b2422bd5c1100
Advaith GS <advaith.gs4@gmail.com>
2023-09-26 12:46:39 +0530

    Update presence.py

--- bot/assets/loops/presence.py
+++ bot/assets/loops/presence.py
@@ -2,7 +2,7 @@ import disnake
 from disnake.ext import tasks
 from time import strftime
 from itertools import cycle
-from assets.database.database import update
+from assets.database.database import update,retrieve
 
 activities = iter(cycle([[0, 'With the stars'], [2, 'The Sounds Of The Universe'],[3, 'Cosmos'], [0, 'With a bunch of Neutron stars'], [2, '/help'],[3, 'How The Universe Works'],[0, 'Life of A Star'],[2, 'Richard Feynman talk about bongos'], [3, 'Milky Way and Andromeda collide'], [3,'The James Webb Space Telescope'], [2, 'Your .iss requests' ]]))
 
@@ -13,10 +13,9 @@ async def set_activity(client,caller):
   activity = next(activities)
   #0 - playing 1- playing and twitch  2 - Listening 3 - Watching 4 -  5- competing
 
-  with open('log.txt','a') as f:
-    f.write(f'\n{strftime("%d/%m/%Y-%H:%M")} {caller}: {activity[0]}-{activity[1]}')
-  with open('log.txt','r') as f:
-    update(f.read(),'logs')
+  x = retrieve('logs')
+  x += '\n{strftime("%d/%m/%Y-%H:%M")} {caller}: {activity[0]}-{activity[1]}'
+  update(f.read(),'logs')
   await client.change_presence(status = disnake.Status.idle,activity = disnake.Activity(name = activity[1],type = activity[0]))
 
 def call_set_activity(client,caller = "Automatic"):
