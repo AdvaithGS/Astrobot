16f6a3749895571db54163a08956ea56e7b4aaff
Advaith GS <advaith.gs4@gmail.com>
2023-09-26 12:54:45 +0530

    Update presence.py

--- bot/assets/loops/presence.py
+++ bot/assets/loops/presence.py
@@ -2,7 +2,6 @@ import disnake
 from disnake.ext import tasks
 from time import strftime
 from itertools import cycle
-from assets.database.database import update,retrieve
 
 activities = iter(cycle([[0, 'With the stars'], [2, 'The Sounds Of The Universe'],[3, 'Cosmos'], [0, 'With a bunch of Neutron stars'], [2, '/help'],[3, 'How The Universe Works'],[0, 'Life of A Star'],[2, 'Richard Feynman talk about bongos'], [3, 'Milky Way and Andromeda collide'], [3,'The James Webb Space Telescope'], [2, 'Your .iss requests' ]]))
 
@@ -12,10 +11,6 @@ async def set_activity(client,caller):
 
   activity = next(activities)
   #0 - playing 1- playing and twitch  2 - Listening 3 - Watching 4 -  5- competing
-
-  x = retrieve('logs')
-  x += '\n{strftime("%d/%m/%Y-%H:%M")} {caller}: {activity[0]}-{activity[1]}'
-  update(x,'logs')
   await client.change_presence(status = disnake.Status.idle,activity = disnake.Activity(name = activity[1],type = activity[0]))
 
 def call_set_activity(client,caller = "Automatic"):
