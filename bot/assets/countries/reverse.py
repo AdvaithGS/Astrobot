611dd7347ddbda2b42745d23bc47baea559f54a8
= <advaith.gs4@gmail.com>
2024-11-30 13:58:25 +0530

    modified reverse.py

--- bot/assets/countries/reverse.py
+++ bot/assets/countries/reverse.py
@@ -2,11 +2,15 @@ from requests import get
 from random import random,choice
 def reverse_geocode(lat : float,lon : float):
  lat,lon = round(lat,3),round(lon,3)
- req = get(f'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}&zoom=10&accept-language=en').json()
+ req = get(f'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}&zoom=10&accept-language=en')
  var = 2
- while req == {'error': 'Unable to geocode'}:
+ 
+ if(req.status_code == 403):
+  print(req.content)
+
+ while req.json() == {'error': 'Unable to geocode'}:
   var += 1
-  req = get(f'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat + choice([-var,+var])*random()}&lon={lon + choice([-var,+var])*random()}&zoom=10&accept-language=en').json()
- print(req['lat'],req['lon'])
+  req = get(f'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat + choice([-var,+var])*random()}&lon={lon + choice([-var,+var])*random()}&zoom=10&accept-language=en')
+ print(req.json()['lat'],req.json()['lon'])
  return req['display_name']
 
