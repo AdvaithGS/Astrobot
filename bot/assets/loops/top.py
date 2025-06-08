3be2e9e35b16ef01dcb235ea68c6eab2045a790b
Advaith GS <advaith.gs4@gmail.com>
2025-06-08 14:09:23 +0530

    dont update guilds

--- bot/assets/loops/top.py
+++ bot/assets/loops/top.py
@@ -3,8 +3,4 @@ import topgg
 from os import environ
 
 async def update_guilds(client):
-  try:
-    dbl_token = environ['TOPGG']
-    client.topggpy = topgg.DBLClient(client, dbl_token,autopost = True)
-  except:
-    pass
+  pass
